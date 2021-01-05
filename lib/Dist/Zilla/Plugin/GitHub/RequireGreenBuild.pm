18aef59396a24a273c69089184f9f85a599c1c36
Graham TerMarsch <graham@howlingfrog.com>
2021-01-04 22:35:20 -0800

    Release - v0.01

--- lib/Dist/Zilla/Plugin/GitHub/RequireGreenBuild.pm
+++ lib/Dist/Zilla/Plugin/GitHub/RequireGreenBuild.pm
@@ -4,19 +4,29 @@ use Moose;
 extends 'Dist::Zilla::Plugin::GitHub';
 with qw(
   Dist::Zilla::Role::BeforeRelease
-  Dist::Zilla::Role::Git::Repo
 );
+use Git::Wrapper;
 use HTTP::Tiny;
 use List::Util qw(first);
 use namespace::clean;
 
 our $VERSION = '0.01';
 
+has repo_root => (
+  is         => 'ro',
+  isa        => 'Str',
+  lazy_build => 1,
+);
+sub _build_repo_root {
+  return shift->zilla->root->stringify;
+}
+
 sub before_release {
   my $self = shift;
 
   # What is the "HEAD" commit that we're attempting to release?
-  my ($head_sha) = $self->git->rev_parse('HEAD');
+  my $git = Git::Wrapper->new($self->repo_root);
+  my ($head_sha) = $git->rev_parse('HEAD');
 
   # What GitHub repo should we be querying?
   my $repo_name = $self->_get_repo_name;
@@ -38,7 +48,6 @@ sub before_release {
   while (1) {
     my $url = $self->api . "/repos/$repo_name/actions/runs?per_page=$per_page&page=$page";
     my $res = $http->request('GET', $url, { headers => $self->_auth_headers });
-$self->log_debug($url);
 
     my $data = $self->_check_response($res);
     unless ($data) {
@@ -57,7 +66,7 @@ $self->log_debug($url);
       $page++;
       next;
     }
-    $self->log_debug("found workflow runs for this SHA");
+    $self->log_debug("found workflow runs for $head_sha");
 
     # ... was the Workflow Run complete?
     unless ($run->{status} eq 'completed') {
@@ -86,13 +95,8 @@ Dist::Zilla::Plugin::GitHub::RequireGreenBuild - Require a successful GitHub Act
 
 =head1 SYNOPSIS
 
-  Configure Git with your GitHub login name:
-
-    $ git config --global github.user YourLoginUsername
-
-  then in your dist.ini:
-
-    [GitHub::RequireGreenBuild]
+  # in your dist.ini:
+  [GitHub::RequireGreenBuild]
 
 =head1 DESCRIPTION
 
@@ -102,13 +106,20 @@ before allowing a release.
 e.g. until we can determine that you have a green build for a GitHub Actions run
 against C<HEAD>, you're not allowed to release.
 
+To determine whether or not a successful run exists, we page through all of
+your GitHub Actions runs, to find a successful run for the current C<HEAD> SHA.
+
+This comes with the implicit expectation that you have already pushed your
+commits back up to GitHub.  If you haven't pushed, we find any runs matching the
+current C<HEAD>, and you won't be able to release.
+
 =head1 AUTHOR
 
 Graham TerMarsch (cpan@howlingfrog.com)
 
 =head1 COPYRIGHT
 
-Copyright (C) 2020-, Graham TerMarsch.  All Rights Reserved.
+Copyright (C) 2021-, Graham TerMarsch.  All Rights Reserved.
 
 This is free software; you can redistribute it and/or modify it under the same
 license as Perl itself.
@@ -118,6 +129,8 @@ license as Perl itself.
 =over
 
 =item L<Dist::Zilla>
+
+=item L<Dist::Zilla::Plugin::GitHub>
 
 =back
 
